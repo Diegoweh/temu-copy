01523d8b06b707c002bdd84b8574865dac8b2b9b
Diegoweh <diegocelaia@outlook.com>
2025-04-30 15:34:51 -0700

    Sanity CMS Setup + Add products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import { getCurrentSession } from "@/actions/auth";
+import { getAllProducts } from "@/sanity/lib/client";
+import { urlFor } from "@/sanity/lib/image";
 
 const Home = async () => {
-
   const { user } = await getCurrentSession();
+  const products = await getAllProducts();
 
   return (
-    <div className="">
-      {JSON.stringify(user)}
+    <div className="grid grid-cols-3 gap-4">
+      {products.map((product) => (
+        <div key={product._id} className="p-4 border">
+          <h2>{product.title}</h2>
+          {product.image && <img src={urlFor(product.image).url()} alt={product.title} />}
+          <p>{product.description}</p>
+          <p>Price: {product.price}</p>
+        </div>
+      ))}
     </div>
   );
 }
