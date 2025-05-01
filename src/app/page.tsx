713c8e7c19b1af8a80d9da26928c8f3ef55cffa2
Diegoweh <diegocelaia@outlook.com>
2025-04-30 17:00:59 -0700

    Home Page finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { getCurrentSession } from "@/actions/auth";
+import SalesCampaignBanner from "@/components/layout/SalesCampaignBanner";
+import ProductGrid from "@/components/product/ProductGrid";
 import { getAllProducts } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
 
@@ -7,15 +9,12 @@ const Home = async () => {
   const products = await getAllProducts();
 
   return (
-    <div className="grid grid-cols-3 gap-4">
-      {products.map((product) => (
-        <div key={product._id} className="p-4 border">
-          <h2>{product.title}</h2>
-          {product.image && <img src={urlFor(product.image).url()} alt={product.title} />}
-          <p>{product.description}</p>
-          <p>Price: {product.price}</p>
-        </div>
-      ))}
+    <div>
+      <SalesCampaignBanner />
+
+      <section className='container mx-auto py-8'>
+            <ProductGrid products={products} />
+          </section>
     </div>
   );
 }
